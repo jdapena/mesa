c91db1cf0fe6ac452b2ab9f94a6bdb536b836829
Alejandro Piñeiro <apinheiro@igalia.com>
2015-06-16 17:08:04 +0200

    i965/nir/vec4: Add setup for system values
    Similar to other variable setups, system values will initialize the
    corresponding register inside a 'nir_system_values' map, which will then
    be queried later when processing the different system value intrinsics
    for the appropriate register.

--- src/mesa/drivers/dri/i965/brw_vec4.h
+++ src/mesa/drivers/dri/i965/brw_vec4.h
@@ -392,6 +392,7 @@ public:
    virtual void nir_setup_uniforms(nir_shader *shader);
    virtual void nir_setup_uniform(nir_variable *var);
    virtual void nir_setup_builtin_uniform(nir_variable *var);
+   virtual void nir_setup_system_value_intrinsic(nir_intrinsic_instr *instr);
    virtual void nir_setup_system_values(nir_shader *shader);
    virtual void nir_emit_impl(nir_function_impl *impl);
    virtual void nir_emit_cf_list(exec_list *list);
@@ -411,6 +412,7 @@ public:
    src_reg *nir_inputs;
    brw_reg_type *nir_output_types;
    unsigned *nir_uniform_driver_location;
+   dst_reg *nir_system_values;
 
 protected:
    void emit_vertex();
