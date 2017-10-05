d3010d379513d8a7196ce52273ce7e4080a8839d
Alejandro Piñeiro <apinheiro@igalia.com>
2017-10-05 16:17:47 +0200

    nir/spirv: add gl_spirv_validation method
    ARB_gl_spirv adds the ability to use SPIR-V binaries, and a new
    method, glSpecializeShader. From OpenGL 4.6 spec, section 7.2.1
    "Shader Specialization", error table:
    
       INVALID_VALUE is generated if <pEntryPoint> does not name a valid
       entry point for <shader>.
    
       INVALID_VALUE is generated if any element of <pConstantIndex>
       refers to a specialization constant that does not exist in the
       shader module contained in <shader>.""
    
    But we are not really interested on creating the nir shader at that
    point, and adding nir structures on the gl_program, so at that point
    we are just interested on the error checking.
    
    So we add a new method focused on just checking those errors. It still
    needs to parse the binary, but skips what it is not needed, and
    doesn't create the nir shader.
    
    v2:
      * Rebase update

--- src/compiler/spirv/nir_spirv.h
+++ src/compiler/spirv/nir_spirv.h
@@ -41,7 +41,13 @@ struct nir_spirv_specialization {
       uint32_t data32;
       uint64_t data64;
    };
+   bool defined_on_module;
 };
+
+
+bool gl_spirv_validation(const uint32_t *words, size_t word_count,
+                         struct nir_spirv_specialization *spec, unsigned num_spec,
+                         gl_shader_stage stage, const char *entry_point_name);
 
 nir_function *spirv_to_nir(const uint32_t *words, size_t word_count,
                            struct nir_spirv_specialization *specializations,
