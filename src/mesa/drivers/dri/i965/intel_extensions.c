259be1c28f4dc19ad299c086ae655850b1f5d787
Alejandro Piñeiro <apinheiro@igalia.com>
2017-12-05 15:29:43 +0100

    i965: enable ARB_spirv_extensions for gen8+

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -280,6 +280,7 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.ARB_gpu_shader_int64 = true;
       ctx->Extensions.ARB_shader_ballot = true; /* requires ARB_gpu_shader_int64 */
       ctx->Extensions.ARB_ES3_2_compatibility = true;
+      ctx->Extensions.ARB_spirv_extensions = true;
    }
 
    if (devinfo->gen >= 9) {
