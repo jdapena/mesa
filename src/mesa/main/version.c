yuq/mesa-lima

commit a0c8b49284efe736849c0a45920ad0a1bbd8d93d

mesa: enable OpenGL 3.1 with ARB_compatibility
Tested-by: Dieter Nützel <Dieter@nuetzel-hh.de>
Reviewed-by: Brian Paul <brianp@vmware.com>

src/mesa/main/version.c
@@ -580,11 +580,11 @@ _mesa_get_version(const struct gl_extensions *extensions,
 {
    switch (api) {
    case API_OPENGL_COMPAT:
-      /* Disable GLSL 1.40 and later for legacy contexts.
-       * This disallows creation of the GL 3.1 compatibility context. */
+      /* Disable higher GLSL versions for legacy contexts.
+       * This disallows creation of higher compatibility contexts. */
       if (!consts->AllowHigherCompatVersion) {
-         if (consts->GLSLVersion > 130) {
-            consts->GLSLVersion = 130;
+         if (consts->GLSLVersion > 140) {
+            consts->GLSLVersion = 140;
          }
       }
       /* fall through */
@@ -607,7 +607,7 @@ void
 _mesa_compute_version(struct gl_context *ctx)
 {
    if (ctx->Version)
-      return;
+      goto done;
 
    ctx->Version = _mesa_get_version(&ctx->Extensions, &ctx->Const, ctx->API);
    ctx->Extensions.Version = ctx->Version;
@@ -615,7 +615,7 @@ _mesa_compute_version(struct gl_context *ctx)
    /* Make sure that the GLSL version lines up with the GL version. In some
     * cases it can be too high, e.g. if an extension is missing.
     */
-   if (ctx->API == API_OPENGL_CORE) {
+   if (_mesa_is_desktop_gl(ctx) && ctx->Version >= 31) {
       switch (ctx->Version) {
       case 31:
          ctx->Const.GLSLVersion = 140;
@@ -651,6 +651,10 @@ _mesa_compute_version(struct gl_context *ctx)
       create_version_string(ctx, "OpenGL ES ");
       break;
    }
+
+done:
+   if (ctx->API == API_OPENGL_COMPAT && ctx->Version >= 31)
+      ctx->Extensions.ARB_compatibility = GL_TRUE;
 }
 
 

docs/relnotes/18.1.0.html
@@ -26,8 +26,8 @@ Mesa 18.1.0 implements the OpenGL 4.5 API, but the version reported by
 glGetString(GL_VERSION) or glGetIntegerv(GL_MAJOR_VERSION) /
 glGetIntegerv(GL_MINOR_VERSION) depends on the particular driver being used.
 Some drivers don't support all the features required in OpenGL 4.5.  OpenGL
-4.5 is <strong>only</strong> available if requested at context creation
-because compatibility contexts are not supported.
+4.5 is <strong>only</strong> available if requested at context creation.
+Compatibility contexts may report a lower version depending on each driver.
 </p>
 
 
@@ -44,6 +44,7 @@ Note: some of the new features are only available with certain drivers.
 </p>
 
 <ul>
+<li>OpenGL 3.1 with ARB_compatibility on nv50, nvc0, r600, radeonsi, softpipe, llvmpipe, svga</li>
 <li>GL_ARB_bindless_texture on nvc0/maxwell+</li>
 <li>GL_EXT_semaphore on radeonsi</li>
 <li>GL_EXT_semaphore_fd on radeonsi</li>

src/mesa/drivers/dri/common/dri_util.c
@@ -381,14 +381,6 @@ driCreateContextAttribs(__DRIscreen *screen, int api,
 	}
     }
 
-    /* Mesa does not support the GL_ARB_compatibilty extension or the
-     * compatibility profile.  This means that we treat a API_OPENGL_COMPAT 3.1 as
-     * API_OPENGL_CORE and reject API_OPENGL_COMPAT 3.2+.
-     */
-    if (mesa_api == API_OPENGL_COMPAT &&
-        ctx_config.major_version == 3 && ctx_config.minor_version == 1)
-       mesa_api = API_OPENGL_CORE;
-
     if (mesa_api == API_OPENGL_COMPAT
         && ((ctx_config.major_version > 3)
             || (ctx_config.major_version == 3 &&

jdapena/mesa

commit ac0ef1e34aa465891bc9c94b983eb67585ff876f

mesa: override to GL core API only when >= 3.2
Fixes: a0c8b49284e ("mesa: enable OpenGL 3.1 with ARB_compatibility")

docs/envvars.html
@@ -91,16 +91,16 @@ glGetString(GL_VERSION) and possibly the GL API type.
 <li> The format should be MAJOR.MINOR[FC]
 <li> FC is an optional suffix that indicates a forward compatible context.
 This is only valid for versions &gt;= 3.0.
-<li> GL versions &lt; 3.0 are set to a compatibility (non-Core) profile
-<li> GL versions = 3.0, see below
-<li> GL versions &gt; 3.0 are set to a Core profile
-<li> Examples: 2.1, 3.0, 3.0FC, 3.1, 3.1FC
+<li> GL versions &lt;= 3.1 are set to a compatibility (non-Core) profile
+<li> GL versions &gt; 3.1 are set to a Core profile
+<li> Examples: 2.1, 3.0, 3.1, 3.1FC, 3.2, 3.2FC
 <ul>
 <li> 2.1 - select a compatibility (non-Core) profile with GL version 2.1
 <li> 3.0 - select a compatibility (non-Core) profile with GL version 3.0
-<li> 3.0FC - select a Core+Forward Compatible profile with GL version 3.0
-<li> 3.1 - select a Core profile with GL version 3.1
+<li> 3.1 - select a compatibility (non-Core) profile with GL version 3.1
 <li> 3.1FC - select a Core+Forward Compatible profile with GL version 3.1
+<li> 3.2 - select a Core profile with GL version 3.2
+<li> 3.2FC - select a Core+Forward Compatible profile with GL version 3.2
 </ul>
 <li> Mesa may not really implement all the features of the given version.
 (for developers only)

src/mesa/main/version.c
@@ -141,9 +141,10 @@ create_version_string(struct gl_context *ctx, const char *prefix)
  *
  * 2.1: select a compatibility (non-Core) profile with GL version 2.1
  * 3.0: select a compatibility (non-Core) profile with GL version 3.0
- * 3.0FC: select a Core+Forward Compatible profile with GL version 3.0
- * 3.1: select a Core profile with GL version 3.1
+ * 3.1: select a compatibility (non-Core) profile with GL version 3.1
  * 3.1FC: select a Core+Forward Compatible profile with GL version 3.1
+ * 3.2: select a Core profile with GL version 3.2
+ * 3.2FC: select a Core+Forward Compatible profile with GL version 3.2
  */
 bool
 _mesa_override_gl_version_contextless(struct gl_constants *consts,
@@ -165,7 +166,7 @@ _mesa_override_gl_version_contextless(struct gl_constants *consts,
          if (version >= 30 && fwd_context) {
             *apiOut = API_OPENGL_CORE;
             consts->ContextFlags |= GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT;
-         } else if (version >= 31 && !compat_context) {
+         } else if (version >= 32 && !compat_context) {
             *apiOut = API_OPENGL_CORE;
          } else {
             *apiOut = API_OPENGL_COMPAT;